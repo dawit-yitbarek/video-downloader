212c6763a9afa1bf0f4f3df9c4a9855a3c82f772
dawit-yitbarek <dawityit17@gmail.com>
2025-12-07 01:30:09 +0300

    Added a health check endpoint

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import bot from "./controllers/videoDownloader.js";
 import { PORT } from "./config/env.js";
+import { ok } from "assert";
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+app.get('/health', (req, res) => res.send('OK'));
 
 (async () => {
     try {
