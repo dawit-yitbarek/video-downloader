f79e26dd2dd3ebcfa95ee63b8cb636c2ce0608a2
dawit-yitbarek <dawityit17@gmail.com>
2025-12-11 01:12:27 +0300

    Modify the start script to install gdown correctly on render environment

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-# 1. Load Puppeteer profiles
+# Create venv
+python3 -m venv venv
+source venv/bin/activate
+
+# Install gdown inside venv
+pip install --no-cache-dir gdown --quiet
+
+# Load Puppeteer profiles
 mkdir -p ./src/bin
 if [ -n "$FILE_ID" ]; then
-    # Install gdown if not already present
-    pip install --no-cache-dir gdown --quiet
-
     echo "Downloading puppeteer-profiles.zip from Google Drive..."
     gdown "https://drive.google.com/uc?id=$FILE_ID" -O puppeteer-profiles.zip
 
@@ -23,7 +27,7 @@ else
     exit 1
 fi
 
-# 2. Download latest static ffmpeg build
+# Download latest static ffmpeg build
 echo "Downloading FFmpeg..."
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
 tar -xf ffmpeg.tar.xz
@@ -33,11 +37,11 @@ chmod +x ./src/bin/ffmpeg ./src/bin/ffprobe
 rm -rf ffmpeg-*-amd64-static ffmpeg.tar.xz
 echo "FFmpeg ready ✔"
 
-# 3. Download Linux yt-dlp binary
+# Download Linux yt-dlp binary
 echo "Downloading yt-dlp..."
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./src/bin/yt-dlp
 chmod +x ./src/bin/yt-dlp
 echo "yt-dlp ready ✔"
 
-# 4. Start the Node server
+# Start Node server
 node ./src/server.js
